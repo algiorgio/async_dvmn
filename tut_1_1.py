a61f91d4c2afe6111cace51f6b90a05282a51618
g.alexandrov <g.alexandrov@unn.ooo>
2019-12-02 15:03:55 +0300

    Lesson 1. Part 3

--- tut_1_1.py
+++ tut_1_1.py
@@ -2,13 +2,32 @@ import time
 import curses
 
 
-def draw(canvas):
-    row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello, World!')
-    canvas.refresh()
-    time.sleep(1)
+def animate(canvas):
+    while True:
+        canvas.refresh()
+        canvas.border()
+        canvas.refresh()
+        curses.curs_set(False)
+
+        row, column = (5, 20)
+
+        canvas.addstr(row, column, '*', curses.A_DIM)
+        time.sleep(2)
+        canvas.refresh()
+
+        canvas.addstr(row, column, '*')
+        time.sleep(0.3)
+        canvas.refresh()
+
+        canvas.addstr(row, column, '*', curses.A_BOLD)
+        time.sleep(0.5)
+        canvas.refresh()
+
+        canvas.addstr(row, column, '*')
+        time.sleep(0.5)
+        canvas.refresh()
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
-    curses.wrapper(draw)
+    curses.wrapper(animate)

7c07d04701c3f86968515a55b1b79be1333df602
g.alexandrov <g.alexandrov@unn.ooo>
2019-12-04 16:22:57 +0300

    coroutine blinking

--- tut_1_1.py
+++ tut_1_1.py
@@ -1,33 +1,69 @@
+import asyncio
 import time
 import curses
 
 
-def animate(canvas):
+def blink_old(canvas):
+    while True:
+        try:
+            canvas.refresh()
+            canvas.border()
+            curses.curs_set(False)
+
+            row, column = (5, 20)
+
+            canvas.addstr(row, column, '*', curses.A_DIM)
+            time.sleep(2)
+            canvas.refresh()
+
+            canvas.addstr(row, column, '*')
+            time.sleep(0.3)
+            canvas.refresh()
+
+            canvas.addstr(row, column, '*', curses.A_BOLD)
+            time.sleep(0.5)
+            canvas.refresh()
+
+            canvas.addstr(row, column, '*')
+            time.sleep(0.3)
+            canvas.refresh()
+        except KeyboardInterrupt:
+            raise
+
+
+async def blink(canvas, row, col, symbol='*'):
     while True:
         canvas.refresh()
         canvas.border()
-        canvas.refresh()
         curses.curs_set(False)
 
-        row, column = (5, 20)
-
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        time.sleep(2)
+        canvas.addstr(row, col, symbol, curses.A_DIM)
         canvas.refresh()
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*')
-        time.sleep(0.3)
+        canvas.addstr(row, col, symbol)
         canvas.refresh()
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        time.sleep(0.5)
+        canvas.addstr(row, col, symbol, curses.A_BOLD)
         canvas.refresh()
+        await asyncio.sleep(0)
 
-        canvas.addstr(row, column, '*')
-        time.sleep(0.5)
+        canvas.addstr(row, col, symbol)
         canvas.refresh()
+        await asyncio.sleep(0)
+
+
+def draw(canvas, row, col):
+    coroutine = blink(canvas, row, col)
+    while True:
+        try:
+            coroutine.send(None)
+        except StopIteration:
+            break
+        time.sleep(1)
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
-    curses.wrapper(animate)
+    curses.wrapper(draw, 5, 5)
